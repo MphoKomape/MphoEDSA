8d4c70b66e6e3091f8b47e4fb2adf9c56e3e1443
Mpho Komape <Mphokomape85@gmail.com>
2019-03-19 14:00:12 +0200

    Fixes...

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='mypackage',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/MphoKomape/MphoEDSA/',
+    url='https://github.com/MphoKomape/MphoEDSA.git',
     author='MphoKomape',
     author_email='mphokomape85@gmail.com'
     )
